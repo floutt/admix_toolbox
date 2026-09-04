41b50fcbceaf846784bc4bd9f6481f84c316f956
Oluwatosin Olayinka <demonslayerguy@gmail.com>
2026-09-04 14:45:43 -0400

    added opt functionality

--- src/vcf_pam_conv.c
+++ src/vcf_pam_conv.c
@@ -3,7 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <inttypes.h>
+#include <getopt.h>
 #include <dotgeno.h>
+
+#define PAM_STR_BUF_EXTRA 6
+#define VCF_SAMP_ADJ(x) (x - 9)
 
 char** str_split(char* str, char delim, size_t* n_elems) {
 	size_t str_len = strlen(str);
@@ -67,16 +71,16 @@ void write_snp_and_ind(FILE* f_vcf, char* snp_file, char* ind_file, char** ind_p
 	line[nread - 1] = '\0';
 	size_t n_elems;
 	char** elems = str_split(line, '\t', &n_elems);
-	if((n_elems - 9) != ind_len) {
-		fprintf(stderr, "ERROR: invalid number of individuals provided. Expected %zu, got %zu\n", n_elems - 9, ind_len);
+	if(VCF_SAMP_ADJ(n_elems) != ind_len) {
+		fprintf(stderr, "ERROR: invalid number of individuals provided. Expected %zu, got %zu\n", VCF_SAMP_ADJ(n_elems), ind_len);
 		exit(EXIT_FAILURE);
 	}
 
 	FILE* f_ind = fopen(ind_file, "w+");
 	for(size_t i = 9; i < n_elems; i++) {
 		char* sex = "U";
-		if(ind_sex) { sex = ind_sex[i-9]; }
-		fprintf(f_ind, "%s\t%s\t%s\n", elems[i], sex, ind_pop[i-9]);
+		if(ind_sex) { sex = ind_sex[VCF_SAMP_ADJ(i)]; }
+		fprintf(f_ind, "%s\t%s\t%s\n", elems[i], sex, ind_pop[VCF_SAMP_ADJ(i)]);
 	}
 	fclose(f_ind);
 	for(size_t i = 0; i < n_elems; i++) {
@@ -142,8 +146,8 @@ void write_geno(FILE* f_vcf, snp_data* snp, ind_data* ind, char* geno_file) {
 		if(line[0] == '#') { continue; }
 		size_t n_elems;
 		char** elems = str_split(line, '\t', &n_elems);
-		if((n_elems - 9) != ind->length) {
-			fprintf(stderr, "ERROR: invalid number of individuals provided in VCF reecord. Expected %zu, got %zu\n", n_elems - 9, ind->length);
+		if(VCF_SAMP_ADJ(n_elems) != ind->length) {
+			fprintf(stderr, "ERROR: invalid number of individuals provided in VCF reecord. Expected %zu, got %zu\n", VCF_SAMP_ADJ(n_elems), ind->length);
 			exit(EXIT_FAILURE);
 		}
 
@@ -203,7 +207,7 @@ void write_geno(FILE* f_vcf, snp_data* snp, ind_data* ind, char* geno_file) {
 				fprintf(stderr, "ERROR: invalid value '%s' in sample column of VCF.\n", gt_str);
 				exit(EXIT_FAILURE);
 			}
-			record[i - 9] = dosage;
+			record[VCF_SAMP_ADJ(i)] = dosage;
 
 			// free samp_str_split
 			for(size_t j = 0; j < n_fmt; j++) {
@@ -266,4 +270,202 @@ void write_vcf(pam_file_reader* pfr, char* out_vcf, snp_data* snp, ind_data* ind
 		fprintf(f_out, "\n");
 	}
 	fclose(f_out);
+}
+
+
+void print_help() {
+	printf("Help goes here!\n");
+}
+
+void print_multiple_flag_error(char* flag) {
+	fprintf(stderr, "Error: Multiple instances of %s flag!\n", flag);
+}
+
+int main(int argc, char* argv[]) {
+	char* in_vcf = NULL;
+	char* in_geno = NULL;
+	char* in_ind = NULL;
+	char* in_snp = NULL;
+	bool is_prefix = false;
+	
+	char* out_vcf = NULL;
+	char* out_geno = NULL;
+	char* out_ind = NULL;
+	char* out_snp = NULL;
+
+	char** sex = NULL; size_t n_sex;
+	char** pop = NULL; size_t n_pop;
+
+	static struct option long_options[] = {
+		{"help",        no_argument,       NULL, 'h'},
+		{"vcf",         required_argument, NULL, 'v'},
+		{"geno",        required_argument, NULL, 'g'},
+		{"ind",         required_argument, NULL, 'i'},
+		{"snp",         required_argument, NULL, 's'},
+		{"prefix",      required_argument, NULL, 'p'},
+		{"sex",         required_argument, NULL, 'S'},
+		{"pop",         required_argument, NULL, 'P'},
+		{"out-prefix",  required_argument, NULL, 'o'},
+		{"out-vcf",     required_argument, NULL, 'O'}
+	};
+
+	size_t alloc_size = 0;  // to store for dynamic string creation;
+	while(1) {
+		int c = getopt_long(argc, argv, "hp:P:s:i:o:", long_options, NULL);
+		if(c == -1) { break; }
+		switch(c) {
+			case 'h':
+				print_help();
+				exit(EXIT_SUCCESS);
+			case 'v':
+				if(in_vcf) {
+					print_multiple_flag_error("'--vcf'");
+					exit(EXIT_FAILURE);
+				}
+				in_vcf = optarg;
+				break;
+			case 'g':
+				if(in_geno) {
+					print_multiple_flag_error("'--geno' or '--prefix'");
+					exit(EXIT_FAILURE);
+				}
+				in_geno = optarg;
+				break;
+			case 'i':
+				if(in_ind) {
+					print_multiple_flag_error("'--ind' or '--prefix'");
+					exit(EXIT_FAILURE);
+				}
+				in_ind = optarg;
+				break;
+			case 's':
+				if(in_snp) {
+					print_multiple_flag_error("'--snp' or '--prefix'");
+					exit(EXIT_FAILURE);
+				}
+				in_snp = optarg;
+				break;
+			case 'p':
+				if((in_geno != NULL) || (in_snp != NULL) || (in_ind != NULL)) {
+					fprintf(stderr, "ERROR: PACKEDANCESTRYMAP file input information already provided!\n");
+					exit(EXIT_FAILURE);
+				}
+				is_prefix = true;
+				alloc_size = strlen(optarg) + PAM_STR_BUF_EXTRA + 1;
+				in_geno = (char*)malloc(alloc_size * sizeof(char));
+				in_snp = (char*)malloc(alloc_size * sizeof(char));
+				in_ind = (char*)malloc(alloc_size * sizeof(char));
+				sprintf(in_geno, "%s.geno", optarg);
+				sprintf(in_snp, "%s.snp", optarg);
+				sprintf(in_ind, "%s.ind", optarg);
+				break;
+			case 'S':
+				if(sex) {
+					print_multiple_flag_error("'--sex'");
+					exit(EXIT_FAILURE);
+				}
+				sex = str_split(optarg, ',', &n_sex);
+				break;
+			case 'P':
+				if(pop) {
+					print_multiple_flag_error("'--pop'");
+					exit(EXIT_FAILURE);
+				}
+				pop = str_split(optarg, ',', &n_pop);
+				break;
+			case 'o':
+				if((out_geno != NULL) || (out_snp != NULL) || (out_ind != NULL)) {
+					fprintf(stderr, "ERROR: PACKEDANCESTRYMAP genotype file output information already provided!\n");
+					exit(EXIT_FAILURE);
+				}
+				alloc_size = strlen(optarg) + PAM_STR_BUF_EXTRA + 1;
+				out_geno = (char*)malloc(alloc_size * sizeof(char));
+				out_snp = (char*)malloc(alloc_size * sizeof(char));
+				out_ind = (char*)malloc(alloc_size * sizeof(char));
+				sprintf(out_geno, "%s.geno", optarg);
+				sprintf(out_snp, "%s.snp", optarg);
+				sprintf(out_ind, "%s.ind", optarg);
+				break;
+			case 'O':
+				if(out_vcf) {
+					print_multiple_flag_error("'--out-vcf'");
+					exit(EXIT_FAILURE);
+				}
+				out_vcf = optarg;
+				break;
+			case '?':
+				break;
+			default:
+				abort();
+		}
+	}
+	// check input integrity
+	bool in_pam = (in_geno != NULL) || (in_snp != NULL) || (in_ind != NULL);
+	bool out_pam = (out_geno != NULL) || (out_snp != NULL) || (out_ind != NULL);
+	if((in_vcf != NULL) && in_pam) {
+		fprintf(stderr, "ERROR: both PACKEDANCESTRYMAP and VCF inputs provided!\n");
+		exit(EXIT_FAILURE);
+	}
+	// check output integrity
+	if((out_vcf != NULL) && out_pam) {
+		fprintf(stderr, "ERROR: both PACKEDANCESTRYMAP and VCF inputs provided!\n");
+		exit(EXIT_FAILURE);
+	}
+	// check input and output alignment
+	if((in_vcf != NULL) && (out_vcf != NULL)) {
+		 fprintf(stderr, "ERROR: VCF input provided alongside VCF output. No conversion can be performed.\n");
+		 exit(EXIT_FAILURE);
+	}
+	if(in_pam && out_pam) {
+		fprintf(stderr, "ERROR: PACKEDANCESTRYMAP input provided alongside PACKEDANCESTRYMAP output. No conversion can be performed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// final run
+	if(in_pam) {
+		ind_data ind = read_ind_file(in_ind);
+		snp_data snp = read_snp_file(in_snp);
+		pam_file_reader pfr = pam_file_reader_init(in_geno, &snp, &ind);
+		read_pam_header(&pfr);	
+		write_vcf(&pfr, out_vcf, &snp, &ind);
+		free_ind_data(&ind);
+		free_snp_data(&snp);
+		close_pam_file_reader(&pfr);
+	} else if(in_vcf != NULL) {
+		// chekc that population information was provided
+		if(pop == NULL) {
+			fprintf(stderr, "ERROR: --pop parameter must be provided to convert VCF to PACKEDANCESTRYMAP.\n");
+			exit(EXIT_FAILURE);
+		}
+		if((sex != NULL) && (n_sex != n_pop)) {
+			fprintf(stderr, "ERROR: invalid number of '--pop' or '--sex' labels provided!\n");
+			exit(EXIT_FAILURE);
+		}
+		FILE* f_vcf = fopen(in_vcf, "r");
+		if(f_vcf == NULL) {
+			fprintf(stderr, "ERROR: could not open VCF file.\n");
+			exit(EXIT_FAILURE);
+		}
+		write_snp_and_ind(f_vcf, out_snp, out_ind, pop, sex, n_pop);
+		ind_data ind = read_ind_file(out_ind);
+		snp_data snp = read_snp_file(out_snp);
+		write_geno(f_vcf, &snp, &ind, out_geno);
+		free_ind_data(&ind);
+		free_snp_data(&snp);
+		fclose(f_vcf);
+	}
+	free(out_geno); free(out_ind); free(out_snp);
+	if(is_prefix) { free(in_geno); free(in_ind); free(in_snp); }
+	if(sex) {
+		for(size_t i = 0; i < n_sex; i++) {
+			free(sex[i]);
+		}
+		free(sex);
+	}
+	if(pop) {
+		for(size_t i = 0; i < n_pop; i++) {
+			free(pop[i]);
+		}
+		free(pop);
+	}
 }
